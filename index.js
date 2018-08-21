ce318a8c44653d588a67a5968d629d47609486b2
gjbonner <gjbonner@gmail.com>
2018-08-21 16:52:07 -0400

    css

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded',() => {
-  const canvas = document.getElementById('background')
   const container = document.getElementById('container')
   // canvas.addEventListener('mousemove', (e) => {
   //   console.log('x', e.clientX, 'y', e.clientY)
@@ -7,61 +6,46 @@ document.addEventListener('DOMContentLoaded',() => {
   let i = 1
 
   function createSad(){
-    container.innerHTML += `<div class='frown' id='frown${i}' style='padding:0'><img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/9632/sad.png'></div>`
+    container.innerHTML += `<div class='frown' id='frown'><img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/9632/sad.png'></div>`
   }
   createSad()
 
-  // console.log(frown.offsetLeft, frown.offsetTop)
-  let randomDirection = ['top', 'bottom', 'left', 'right']
-
-
-  //need to create element at that point
-  setInterval(() => {
-    let randomInt = Math.floor(Math.random()*randomDirection.length)
-    const selectedDirection = randomDirection[randomInt]
-    console.log(randomInt)
-    createSad()
-    // debugger
-    myMove(selectedDirection)
-  }, 1000)
-
-  // inputting left will move right etc.
-  function myMove(direction) {
-    this.direction = direction.toUpperCase()
-    let frown = document.getElementById(`frown${i}`)
-    i++
-    // console.log(frown)
-    // debugger
-    var pos = 0;
-    var id = setInterval(() => {
-      if (pos > 500) {
-        clearInterval(id);
-        frown.remove();
-      } else {
-        pos++;
-        if(this.direction.toLowerCase() === "right"){
-          frown.style.paddingRight = pos + 'px'
-        }else if (this.direction.toLowerCase() === "left"){
-          frown.style.paddingLeft = pos + 'px'
-        }else if(this.direction.toLowerCase() === "bottom"){
-          frown.style.paddingBottom = pos + 'px'
-        }else if(this.direction.toLowerCase() === "top"){
-          frown.style.paddingTop = pos + 'px'
-        }
-      }
-    }, .5);
-  }
+  //
+  // // console.log(frown.offsetLeft, frown.offsetTop)
+  // let randomDirection = ['top', 'bottom', 'left', 'right']
+  //
+  //
+  // //need to create element at that point
+  // setInterval(() => {
+  //   let randomInt = Math.floor(Math.random()*randomDirection.length)
+  //   const selectedDirection = randomDirection[randomInt]
+  //   createSad()
+  //   myMove(selectedDirection)
+  // }, 2000)
+  //
+  // // inputting left will move right etc.
+  // function myMove(direction) {
+  //   let pos = 0
+  //   this.direction = direction.toUpperCase()
+  //   let frown = document.getElementById(`frown${i}`)
+  //   i++
+  //   let id = setInterval(() => {
+  //     if (pos === 800) {
+  //       clearInterval(id)
+  //       container.innerHTML = ''
+  //     } else {
+  //       pos++
+  //       if(this.direction.toLowerCase() === "right"){
+  //         frown.style.paddingRight = pos + 'px'
+  //       }else if (this.direction.toLowerCase() === "left"){
+  //         frown.style.paddingLeft = pos + 'px'
+  //       }else if(this.direction.toLowerCase() === "bottom"){
+  //         frown.style.paddingBottom = pos + 'px'
+  //       }else if(this.direction.toLowerCase() === "top"){
+  //         frown.style.paddingTop = pos + 'px'
+  //       }
+  //     }
+  //   }, 1)
+  // }
 
 })
-
-// function frame() {
-//   const padding = frown.style.padding`${this.direction}`
-//   if (pos == 500) {
-//     clearInterval(id);
-//   } else {
-//     pos++;
-//     padding = pos + 'px'
-//     // frown.style.paddingLeft = pos + 'px'
-//     // console.log(frown.style.paddingRight)
-//   }
-// }
