1171af9dee9ddd6d1280cce8cb31cd535d1bb9e6
gjbonner <gjbonner@gmail.com>
2018-08-21 13:52:27 -0400

    w

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 document.addEventListener('DOMContentLoaded',() => {
   const canvas = document.getElementById('background')
-  const frown = document.getElementById('frown')
-  console.log(frown.offsetLeft, frown.offsetTop)
   // canvas.addEventListener('mousemove', (e) => {
   //   console.log('x', e.clientX, 'y', e.clientY)
   // })
+  console.log(frown.offsetLeft, frown.offsetTop)
 
-  // moveDown()
-  // function moveDown(){
-  //   let pos = 0
-  //   let id = setInterval(frame, 5)
-  //   function frame(){
-  //     if(pos == 350){
-  //       clearInterval(id)
-  //     } else {
-  //       pos++
-  //       frown.style.top = pos + 'px';
-  //       frown.style.left = pos + 'px';
-  //     }
-  //   }
-  // }
+  function myMove() {
+    let frown = document.getElementById("frown");
+
+    var pos = 0;
+    var id = setInterval(frame, 5);
+    function frame() {
+      if (pos == 500) {
+        clearInterval(id);
+      } else {
+        pos++;
+        frown.style.paddingTop = pos + 'px'
+        frown.style.paddingLeft = pos + 'px'
+        console.log(frown.style.margin)
+      }
+    }
+  }
+  myMove()
 })
